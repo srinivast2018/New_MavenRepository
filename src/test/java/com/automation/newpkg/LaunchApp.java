ca504328b216ce98b70e5fdedc5224271c0e91f0
srinivast2018 <srinivas.nirmal@gmail.com>
2020-06-13 11:32:25 +0530

    Committing First

---
+++ src/test/java/com/automation/newpkg/LaunchApp.java
@@ -0,0 +1,21 @@
+package com.automation.newpkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LaunchApp {
+
+	@Test
+	public void launch()
+	{
+		//System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "./Drivers/geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+	}	
+	
+}
