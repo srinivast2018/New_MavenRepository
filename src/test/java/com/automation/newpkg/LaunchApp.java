528571c20344b44bb0dc394acdf044fe4c125b80
srinivast2018 <srinivas.nirmal@gmail.com>
2020-06-18 15:49:02 +0530

    Updated print statement

--- src/test/java/com/automation/newpkg/LaunchApp.java
+++ src/test/java/com/automation/newpkg/LaunchApp.java
@@ -17,7 +17,7 @@ public class LaunchApp {
 		driver.get("https://www.google.com");
 		System.out.println("Launched portal");
 		driver.manage().window().maximize();
-		System.out.println("Its time to login");
+		System.out.println("Its time to login to the application");
 	}	
 	
 }
